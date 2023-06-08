290e89640cc96063732f43d386b8ce7a71de24c6
Rahul kumar hotta <rahulkumar.hotta@corp.knowledgelens.com>
2023-06-08 17:10:56 +0530

    added header and made rough structure of recipe list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit{
+
+ recipes: Recipe[] = [
+  new Recipe("Test Recipe", "This is a test Recipe", "https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000")
+ ]
+
+ strings : string[] = ["helllo","hello2"]
+
+ ngOnInit() {
+     
+ }
+}
